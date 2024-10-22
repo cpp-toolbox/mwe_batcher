313ef2c1c32d70aa8b6cf1fecfb6f4a968bce367
cuppajoeman <ccn@cuppajoeman.com>
2024-10-22 19:49:25 -0400

    feat: initial working batcher

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include "window/window.hpp"
 #include "shader_cache/shader_cache.hpp"
+#include "batcher/generated/batcher.hpp"
 
 #include <cstdio>
 #include <cstdlib>
@@ -22,69 +23,41 @@ struct OpenGLDrawingData {
     GLuint vao_name;
 };
 
-OpenGLDrawingData prepare_drawing_data_and_opengl_drawing_data(ShaderCache &shader_cache) {
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    float vertices[] = {
-        0.5f,  0.5f,  0.0f, // top right
-        0.5f,  -0.5f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f,  0.0f  // top left
-    };
-    GLuint indices[] = {
-        // note that we start from 0!
-        0, 1, 3, // first Triangle
-        1, 2, 3  // second Triangle
-    };
+std::vector<glm::vec3> vertices_left = {
+    glm::vec3(-0.8f, 0.5f, 0.0f),  // top right of left square
+    glm::vec3(-0.8f, -0.5f, 0.0f), // bottom right of left square
+    glm::vec3(-0.4f, -0.5f, 0.0f), // bottom left of left square
+    glm::vec3(-0.4f, 0.5f, 0.0f)   // top left of left square
+};
 
-    // vbo: vertex buffer object
-    // vao: vertex array object
-    // ibo: index buffer object
+std::vector<glm::vec3> vertices_right = {
+    glm::vec3(0.4f, 0.5f, 0.0f),  // top right of right square
+    glm::vec3(0.4f, -0.5f, 0.0f), // bottom right of right square
+    glm::vec3(0.8f, -0.5f, 0.0f), // bottom left of right square
+    glm::vec3(0.8f, 0.5f, 0.0f)   // top left of right square
+};
 
-    GLuint vbo_name, vao_name, ibo_name;
-
-    glGenVertexArrays(1, &vao_name);
-    glGenBuffers(1, &vbo_name);
-    glGenBuffers(1, &ibo_name);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and
-    // then configure vertex attributes(s).
-    glBindVertexArray(vao_name);
-
-    glBindBuffer(GL_ARRAY_BUFFER, vbo_name);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo_name);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    shader_cache.configure_vertex_attributes_for_drawables_vao(
-        vao_name, vbo_name, ShaderType::ABSOLUTE_POSITION_WITH_SOLID_COLOR, ShaderVertexAttributeVariable::POSITION);
-
-    // note that this is allowed, the call to glVertexAttribPointer registered
-    // vbo_name as the vertex attribute's bound vertex buffer object so afterwards
-    // we can safely unbind
-    /*glBindBuffer(GL_ARRAY_BUFFER, 0);*/
-
-    return {vbo_name, ibo_name, vao_name};
-}
+std::vector<unsigned int> indices = {
+    // note that we start from 0!
+    0, 1, 3, // first Triangle
+    1, 2, 3  // second Triangle
+};
 
 int main() {
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
     console_sink->set_level(spdlog::level::debug);
-
     auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("mwe_shader_cache_logs.txt", true);
     file_sink->set_level(spdlog::level::info);
-
     std::vector<spdlog::sink_ptr> sinks = {console_sink, file_sink};
 
     LiveInputState live_input_state;
-
     GLFWwindow *window = initialize_glfw_glad_and_return_window(&SCREEN_WIDTH, &SCREEN_HEIGHT, "glfw window", false,
                                                                 false, false, &live_input_state);
 
-    std::vector<ShaderType> requested_shaders = {ShaderType::ABSOLUTE_POSITION_WITH_SOLID_COLOR};
+    std::vector<ShaderType> requested_shaders = {ShaderType::ABSOLUTE_POSITION_WITH_SOLID_COLOR,
+                                                 ShaderType::TRANSFORM_V_WITH_TEXTURES};
     ShaderCache shader_cache(requested_shaders, sinks);
-
-    auto [vbo_name, ibo_name, vao_name] = prepare_drawing_data_and_opengl_drawing_data(shader_cache);
+    Batcher batcher(shader_cache);
 
     int width, height;
 
@@ -99,24 +72,15 @@ int main() {
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        shader_cache.use_shader_program(ShaderType::ABSOLUTE_POSITION_WITH_SOLID_COLOR);
-        glBindVertexArray(vao_name); // seeing as we only have a single VAO there's
-                                     // no need to bind it every time, but we'll do
-                                     // so to keep things a bit more organized
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        batcher.absolute_position_with_solid_color_shader_batcher.queue_draw(indices, vertices_left);
+        batcher.absolute_position_with_solid_color_shader_batcher.queue_draw(indices, vertices_right);
+        batcher.absolute_position_with_solid_color_shader_batcher.draw_everything();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwDestroyWindow(window);
-
-    // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &vao_name);
-    glDeleteBuffers(1, &vbo_name);
-    glDeleteBuffers(1, &ibo_name);
-    /*glDeleteProgram(shader_program);*/
 
     glfwTerminate();
     exit(EXIT_SUCCESS);
