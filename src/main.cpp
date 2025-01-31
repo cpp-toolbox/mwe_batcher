999490409741c884bee9715617ca0e86d44d927c
cuppajoeman <ccn@cuppajoeman.com>
2025-01-31 17:03:35 -0500

    chore: update all submodules

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,8 @@ int main() {
     file_sink->set_level(spdlog::level::info);
     std::vector<spdlog::sink_ptr> sinks = {console_sink, file_sink};
 
-    LiveInputState live_input_state;
-    GLFWwindow *window = initialize_glfw_glad_and_return_window(&SCREEN_WIDTH, &SCREEN_HEIGHT, "glfw window", false,
-                                                                false, false, &live_input_state);
+    Window window;
+    window.initialize_glfw_glad_and_return_window(SCREEN_WIDTH, SCREEN_HEIGHT, "glfw window", false, false, false);
 
     std::vector<ShaderType> requested_shaders = {ShaderType::ABSOLUTE_POSITION_WITH_SOLID_COLOR,
                                                  ShaderType::TRANSFORM_V_WITH_TEXTURES};
@@ -39,24 +38,24 @@ int main() {
     glm::vec4 color = glm::vec4(.5, .5, .5, 1);
     shader_cache.set_uniform(ShaderType::ABSOLUTE_POSITION_WITH_SOLID_COLOR, ShaderUniformVariable::RGBA_COLOR, color);
 
-    IndexedVertices ivp = generate_grid(glm::vec3(0, 0, 0), 2, 2, 2, 2, 0.01);
+    IndexedVertices ivp = generate_grid(glm::vec3(0, 0, 0), 2, 2, 50, 50, 0.01);
 
-    while (!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(window.glfw_window)) {
 
-        glfwGetFramebufferSize(window, &width, &height);
+        glfwGetFramebufferSize(window.glfw_window, &width, &height);
 
         glViewport(0, 0, width, height);
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        batcher.absolute_position_with_solid_color_shader_batcher.queue_draw(ivp.indices, ivp.vertices);
+        batcher.absolute_position_with_solid_color_shader_batcher.queue_draw(0, ivp.indices, ivp.vertices);
         batcher.absolute_position_with_solid_color_shader_batcher.draw_everything();
 
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(window.glfw_window);
         glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
+    glfwDestroyWindow(window.glfw_window);
 
     glfwTerminate();
     exit(EXIT_SUCCESS);
